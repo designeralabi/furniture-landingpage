f47031bb325be25198662f5a6070ef5b6c277301
designeralabi <bdlkrmalabi@gmail.com>
2022-01-09 00:57:30 +0100

    rwd incoporated

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,10 @@
 
 $('#image-1').on('click', ()=>{
 
-    if ($('body').hasClass('img-2')){
+    if ($('body').hasClass('img-2') || $('body').hasClass('img-3')){
         $('body').removeClass('img-2');
+        $('body').removeClass('img-3');
     }
-
-    $('#body').addClass('img-1');
     $('.image-change').attr('src', "assets/images/pexels-houzlook-com-3356416.jpg");
 });
 
@@ -15,17 +14,18 @@ $('#image-2').on('click', ()=>{
         $('body').removeClass('img-3');
     }
 
-    $('body').addClass('img-2');
+    $('body').addClass('img-2').fadeIn("fast");
     $('.image-change').attr('src', "assets/images/pexels-mark-mccammon-1080696.jpg");
 });
 
 $('#image-3').on('click', ()=>{
-    $('body').addClass('img-3');
+    if ($('body').hasClass('img-1')){
+        $('body').removeClass('img-1');
+    }
+    $('body').addClass('img-3').fadeIn("fast");
     $('.image-change').attr('src', "assets/images/pexels-thorn-yang-154161.jpg");
 });
 
 
-
-
-
-// .$(selector).hasClass(className);
+// $(selector).fadeIn();
+// $(selector).fadeTo(duration, opacity);
